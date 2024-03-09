269a50d6150c1cbe826afb5ec6b42ddb106528e5
Hamza <hamzakar13y@gmail.com>
2024-03-09 16:09:43 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,40 @@
+﻿using Business.Concrete;
+using Business.Constants;
+using DataAccess.Concrete.Entity_Framework;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        ProductTest();
+        //CategoryTest();
+    }
+
+    private static void CategoryTest()
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        foreach (var category in categoryManager.GetAll())  
+        {
+            Console.WriteLine(category.CategoryName);
+        }
+    }
+
+    private static void ProductTest()
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+        var result = productManager.GetProductDetails();
+        if(result.IsSuccess == true)
+        foreach (var product in result.Data)
+        {
+            Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+        }
+        else
+        {
+            Console.WriteLine(result.Message);
+        }
+
+    }
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess;
+using DataAccess.Concrete.Entity_Framework;
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal : IEntityRepository<Product>
+    {
+       List<ProductDetailDto> GetProductDetails();
+    }
+}
+
+//Code Refactoring
